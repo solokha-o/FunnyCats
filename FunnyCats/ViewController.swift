509fba339cb683abc19cc2a4f47154122c47db8a
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-04-29 16:29:46 +0300

    Initial Commit

---
+++ FunnyCats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunnyCats
+//
+//  Created by Oleksandr Solokha on 29.04.2020.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
