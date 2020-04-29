2e3dc5c036ea4a5e0e4c5ebe37f7093c49edcc93
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-04-29 18:59:36 +0300

    create CatBreedsTableViewController configure NavigationBar title barTintColor create CatBreedsTableViewCell

---
+++ FunnyCats/CatBreedsController/CatBreedsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CatBreedsTableViewCell.swift
+//  FunnyCats
+//
+//  Created by Oleksandr Solokha on 29.04.2020.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import UIKit
+
+class CatBreedsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
